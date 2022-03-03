b2096c615dcfade94b08b1bb4e789a0a4916dc9c
letsgo247 <letsgo247@hanmail.net>
2022-03-03 21:46:20 +0900

    중간 저장

--- web_server.py
+++ web_server.py
@@ -5,6 +5,10 @@ from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
 
+from PIL import Image
+from io import BytesIO
+import base64
+
 UPLOAD_FOLDER = './static/img/in/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -12,7 +16,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def make_finalname(fileObj):
+def make_finalname_list(fileObj):
     filename_encoded = parse.quote(fileObj.filename)    # 한글 에러나서 ?로 인코딩 해주는 함수
     print('# fileObj.name=', filename_encoded)
     filename_cleaned = secure_filename(filename_encoded)     # 알파벳, 대시, 언더바, 닷 등 제거하고 클린하게 만들어줌
@@ -22,9 +26,9 @@ def make_finalname(fileObj):
     time = now.strftime('%Y-%m-%dT%H-%M-%S')
     print('# time=', time)
 
-    finalname = time+'F'+filename_cleaned
+    finalname_list = [time+'F'+filename_cleaned, time+'C'+filename_cleaned]
 
-    return finalname
+    return finalname_list
 
 
 @app.route('/')
@@ -43,9 +47,10 @@ def result():
         # print('# type(fileObj.file)=', type(fileObj.file))
 
         # 경로 형성
-        finalname = make_finalname(fileObj)        
-        in_path = os.path.join(app.config['UPLOAD_FOLDER'], finalname)
-        out_path =os.path.join('./static/img/out/', finalname) 
+        finalname_list = make_finalname_list(fileObj)        
+        in_path = os.path.join(app.config['UPLOAD_FOLDER'], finalname_list[0])
+        out_path =os.path.join('./static/img/out/', finalname_list[0]) 
+        crop_path = os.path.join('./static/img/out/', finalname_list[1])
         
         # 파일 저장
         fileObj.save(in_path)
@@ -57,11 +62,29 @@ def result():
         res = requests.post('http://127.0.0.2:7000/ganarate', files={'img':data})
 
         # 결과 저장
-        file = open(out_path, 'wb')
-        file.write(res.content)
-        file.close()
+        # file = open(out_path, 'wb')
+        # file.write(res.content)
+        
+        # print(res.json()['result'][0])
+        print(len(res.json()['result']))
+        # print(res.content)
+        # print(len(res.content))
+        cropped_img_bytes = res.json()['result'][0]
+        result_img_bytes = res.json()['result'][1]
+        print('cropped_img_bytes:', type(cropped_img_bytes))
 
-        return render_template('result.html', out_path=out_path)
+        cropped_img_data = base64.b64decode(cropped_img_bytes)
+        result_img_data = base64.b64decode(result_img_bytes)
+
+        print('cropped_img_data:', type(cropped_img_data))
+        
+        with open(crop_path, 'wb') as f:
+            f.write(cropped_img_data)
+
+        with open(out_path, 'wb') as f:
+            f.write(result_img_data)
+        
+        return render_template('result.html', out_path=out_path, crop_path=crop_path)
 
 
 if __name__ == '__main__':
